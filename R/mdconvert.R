e7e97b620c99234f6f82a3cb6064c87e7af3f266
Kenji Sato <mail@kenjisato.jp>
2024-05-18 07:54:18 +0900

    Use shinyAce and add Import zip etc.
    * Use {shinyAce} for editor panes.
    * Add functionality to import a zip file.
    * Download zip includes options.
    * Make stylistic changes.
    * Make preprocessing code more modular for unittesting
    * Add several unit tests

--- R/mdconvert.R
+++ R/mdconvert.R
@@ -1,47 +1,78 @@
-# Default documents ----
-.sample_md <- read_pkg_file("text", "sample.md")
-.sample_page_css <- read_pkg_file("css", "article.scss", pkg = "juicedown")
-.sample_label_css <- read_pkg_file("css", "div.scss", pkg = "juicedown")
-
 # UI fragments ----
-
 
 left_panel <- function(.ns) {
   panel_md <- nav_panel("Markdown",
                         class = "ta",
-                        textAreaInput(.ns("md"), NULL, value = .sample_md))
+                        shinyAce::aceEditor(
+                          outputId = .ns("md"), 
+                          value = installed_doc("text", "default"),
+                          mode = "markdown",
+                          fontSize = 13,
+                          height = "100%",
+                          showLineNumbers = FALSE)
+                        )
   panel_css <- nav_panel("CSS",
                          class = "ta",
-                         textAreaInput(.ns("css"), NULL, value = .sample_page_css))
+                         shinyAce::aceEditor(
+                           outputId = .ns("css"), 
+                           value = installed_doc("css", "article"),
+                           mode = "scss",
+                           fontSize = 13,
+                           height = "100%",
+                           showLineNumbers = FALSE)
+                         )
   
   panel_opts <- nav_panel(
     icon("gear"), 
     id = "options-container",
     card(
       fill = FALSE,
-      card_header("Backup and Restore", class = "bg-dark"),
-      downloadLink(.ns("downloadData"), "Download ZIP", icon = icon("download")),
-      actionLink(
-        .ns("default_md"),
-        "Reset to default document",
-        icon = icon("broom")
+      h5("Document"),
+      radioButtons(
+        .ns("type_css"),
+        "Type: ",
+        c("Page" = "page", "Label" = "label"),
+        width = "100%",
+        inline = TRUE
       ),
       layout_column_wrap(
-        width = 1 / 2,
-        actionLink(.ns("default_css"), "Reset to default CSS",
-                   icon = icon("broom")),
-        radioButtons(
-          .ns("type_css"),
-          "Type: ",
-          c("Page" = "page", "Label" = "label"),
-          width = "100%",
-          inline = TRUE
-        )
-      )
+        actionLink(
+          .ns("default_md"),
+          "Reset to default document",
+          icon = icon("broom")
+        ),
+        actionLink(
+          .ns("default_css"), 
+          "Reset to default CSS", 
+          icon = icon("broom")
+        ),
+        width = 1/2
+      ),
+      class = "card-no-border"
     ),
     card(
       fill = FALSE,
-      card_header("Tweaks", class = "bg-dark"),
+      h5("Backup and Restore"),
+      downloadButton(
+        .ns("downloadData"), 
+        "Download ZIP", 
+        icon = icon("download")
+      ),
+      fileInput(
+        .ns("uploadData"), 
+        "Upload ZIP",
+        width = "100%"
+      ),
+      actionButton(
+        .ns("importData"),
+        "Import ZIP",
+        icon = icon("file-import")
+      ),
+      class = "card-no-border"
+    ),
+    card(
+      fill = FALSE,
+      h5("Tweaks"),
       checkboxInput(
         .ns("tweak_h2"),
         "Remove redundant heading (JS)",
@@ -53,7 +84,8 @@ left_panel <- function(.ns) {
         "Fix footnote styling (JS)",
         value = FALSE,
         width = NULL
-      )
+      ),
+      class = "card-no-border"
     )
   )
   
@@ -115,10 +147,13 @@ mdconvertUI <- function(id) {
 mdconvertServer <- function(id, allow_r = TRUE) {
   .tdir <- tempfile(pattern = "dir")
   dir.create(.tdir)
+  
+  # Paths for temporary files
   .md_file <- file.path(.tdir, "page.md")
   .css_file <- file.path(.tdir, "style.scss")
+  .yml_file <- file.path(.tdir, "config.yml")
   .html_file <- file.path(.tdir, "page.html")
-  .files <- c(.md_file, .css_file, .html_file)
+  .files <- c(.md_file, .css_file, .html_file, .yml_file)
   
   moduleServer(id, function(input, output, session) {
     r <- reactiveValues(html = "")
@@ -130,18 +165,19 @@ mdconvertServer <- function(id, allow_r = TRUE) {
     # Convert button
     observeEvent(input$convert, {
       
-      md <- preprocess_md(input, allow_r)
+      md <- preprocess_md(input, !allow_r)
       writeLines(md, .md_file)
       
-      css <- input$css
+      css <- preprocess_css(input) # a workaround a bug of juicedown
       writeLines(css, .css_file)
-      
+
       tag <- if (input$type_css == "page") "article" else "div"
       
       r$html <- juicedown::convert(.md_file,
                                    clip = FALSE,
                                    stylesheet = .css_file,
                                    tag = tag)
+      
       output$preview <-
         renderUI(withMathJax(HTML(paste(
           r$html, collapse = "\n"
@@ -160,18 +196,77 @@ mdconvertServer <- function(id, allow_r = TRUE) {
       },
       content = function(file) {
         
-        md <- preprocess_md(input, allow_r)
+        md <- preprocess_md(input, !allow_r, download = TRUE)
         writeLines(md, .md_file)
         
         css <- input$css
         writeLines(css, .css_file)
         
-        writeLines(r$html, .html_file)
-        utils::zip(file, c(.md_file, .html_file, .css_file), extras = '-j')
+        cfg <- list(
+          type_css = input$type_css,
+          tweak_h2 = input$tweak_h2,
+          tweak_fn = input$tweak_fn
+        )
+        yaml::write_yaml(cfg, .yml_file)
         
+        writeLines(r$html, .html_file)
+        utils::zip(file, c(.md_file, .html_file, .css_file, .yml_file), extras = '-j')
         unlink(.files)
       }
     )
+    
+    # Import Button
+    observeEvent(input$importData, {
+      
+      callback <- function(x) {
+        if (x == FALSE) return(invisible())
+        
+        zipfile <- input$uploadData$datapath[[1]]
+        exdir <- file.path(.tdir, "archive")
+        zip::unzip(zipfile, exdir = exdir)
+        
+        md <- readLines(list.files(exdir, pattern = "\\.R?md", full.names = TRUE)[[1]])
+        md <- paste(md, collapse = "\n")
+        #updateTextAreaInput("md", value = md, session = session)
+        shinyAce::updateAceEditor(session = session, editorId = "md", value = md)
+        
+        css <- readLines(list.files(exdir, pattern = "\\.s?css", full.names = TRUE)[[1]])
+        css <- paste(css, collapse = "\n")
+        #updateTextAreaInput("css", value = css, session = session)
+        shinyAce::updateAceEditor(session = session, editorId = "css", value = css)
+        
+        cfg <- list.files(exdir, pattern = "\\.yml", full.names = TRUE)
+        if (length(cfg) > 0) {
+          yml <- yaml::read_yaml(cfg[[1]])
+          updateRadioButtons("type_css", selected = yml$type_css, session = session)
+          updateCheckboxInput("tweak_h2", value = yml$tweak_h2, session = session)
+          updateCheckboxInput("tweak_fn", value = yml$tweak_fn, session = session)
+        }
+        
+        unlink(zipfile)
+        unlink(exdir)
+      }
+
+      if (is.null(input$uploadData)) {
+        shinyalert::shinyalert(
+          "No file selected!",
+          type = "error"
+        )
+      } else {
+        shinyalert::shinyalert(
+          title = "Caution!",
+          text = "Any changes you made will be discarded! Do you wish to proceed?",
+          size = "s", 
+          type = "warning",
+          showConfirmButton = TRUE,
+          showCancelButton = TRUE,
+          confirmButtonText = "Yes",
+          confirmButtonCol = "#AEDEF4",
+          cancelButtonText = "No",
+          callbackR = callback
+        )
+      }
+    })
     
     # Reset to default
     observeEvent(input$default_md, {
@@ -187,7 +282,9 @@ mdconvertServer <- function(id, allow_r = TRUE) {
         cancelButtonText = "Cancel",
         callbackR = function(x) {
           if (x != FALSE) {
-            updateTextAreaInput(inputId = "md", value = .sample_md)
+            #updateTextAreaInput(inputId = "md", value = installed_doc("text", "default"))
+            shinyAce::updateAceEditor(session = session, editorId = "md",
+                                      value = installed_doc("text", "default"))
           }
         }
       )
@@ -195,9 +292,9 @@ mdconvertServer <- function(id, allow_r = TRUE) {
     
     observeEvent(input$default_css, {
       default_css <- if (input$type_css == "page") {
-        .sample_page_css
+        installed_doc("css", "article")
       } else {
-        .sample_label_css
+        installed_doc("css", "div")
       }
       shinyalert::shinyalert(
         title = "Caution!",
@@ -211,7 +308,9 @@ mdconvertServer <- function(id, allow_r = TRUE) {
         cancelButtonText = "Cancel",
         callbackR = function(x) {
           if (x != FALSE) {
-            updateTextAreaInput(inputId = "css", value = default_css)
+            #updateTextAreaInput(inputId = "css", value = default_css)
+            shinyAce::updateAceEditor(session = session, editorId = "css",
+                                      value = default_css)
           }
         }
       )
--- R/preprocess.R
+++ R/preprocess.R
@@ -1,38 +1,64 @@
+process_command <- function(text) {
+  # Convert app commands into juicedown function calls
+  app_commands <- list(
+    YouTube = "includeYT",
+    Audio = "includeAudio"
+  )
+  
+  for (i in seq_along(app_commands)) {
+    name <- names(app_commands)[[i]]
+    juicedown_name <- app_commands[[i]]
+    from <-  paste0('\\{%\\s*', name, '\\(((\'.*\')|(\".*\"))\\)\\s*%\\}')
+    to <- paste0('\n```{r}\n', juicedown_name, '(\\1)\n```\n')
+    text <- stringr::str_replace(text, from, to)
+  }
+  text
+}
 
-preprocess_md <- function(input, allow_r) {
+process_chunk <- function(text, kill_chunk) {
+  # 
+  if (kill_chunk) {
+    text <- text |>
+      stringr::str_replace_all("```\\{r.*\\}", "```r") |>
+      stringr::str_replace_all("`r(.*)`", "`\\1`")
+  }
+  text
+}
+
+process_options <- function(text, input, download) {
+  if (download) return(text)
+  
+  option_lines <- c(
+    if (input$tweak_h2) "juicedown::tweak_moodle_heading()" else NULL,
+    if (input$tweak_fn) "juicedown::tweak_footnote_highlight()" else NULL
+  )
+
+  if (length(option_lines) > 0) {
+    option_lines <- c("```{r}", option_lines, "```")
+  }
+  c(text, option_lines)
+}
+
+
+preprocess_md <- function(input, kill_chunk, download = FALSE) {
   md <- if (stringr::str_length(input$md) == 0) {
-    .sample_md
+    installed_doc("text", "default")
   } else {
     input$md
   }
   
-  # Escape R chunk
-  if (!allow_r) {
-    md <- md |>
-      stringr::str_replace_all("```\\{r.*\\}", "```r") |>
-      stringr::str_replace_all("`r(.*)`", "`\\1`")
-  }
-  
-  # Special Commands
+  # Process
   md <- md |>
-    stringr::str_replace('\\{%\\s*YouTube\\(((\'.*\')|(\".*\"))\\)\\s*%\\}', 
-                         '\n```{r}\nincludeYT(\\1)\n```\n') |>
-    stringr::str_replace('\\{%\\s*Audio\\(((\'.*\')|(\".*\"))\\)\\s*%\\}', 
-                         '\n```{r}\nincludeAudio(\\1)\n```\n')
-  
-  # Options
-  option_lines <- character(0)
-  if (input$tweak_h2) {
-    option_lines <- c(option_lines, "juicedown::tweak_moodle_heading()")
-  }
-  if (input$tweak_fn) {
-    option_lines <-
-      c(option_lines,
-        "juicedown::tweak_footnote_highlight()")
-  }
-  if (length(option_lines) > 0) {
-    option_lines <- c("```{r}", option_lines, "```")
-  }
-  md <- c(md, option_lines)
-  md
+    process_chunk(kill_chunk) |>
+    process_command() |>
+    process_options(input, download)
 }
+
+preprocess_css <- function(input) {
+  if (input$css == "") {
+    "/* empty */\n"
+  } else {
+    input$css
+  }
+}
+
